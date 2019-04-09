32b8097cddab745aa6edf37919f13d9bc3d98ac6
k3lly.dev <luiskd_98@hotmail.com>
2019-04-09 11:10:23 -0500

    Router Issue

---
+++ build/precache-manifest.81bcebc1dc94d4b9c641d676df33b549.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "436c230ba2d29120ea54",
+    "url": "/static/js/main.c5aeb084.chunk.js"
+  },
+  {
+    "revision": "d5180b39c1a967826de7",
+    "url": "/static/js/2.bd9bd4e0.chunk.js"
+  },
+  {
+    "revision": "451eed613736b2a592402e54d306278d",
+    "url": "/index.html"
+  }
+];
